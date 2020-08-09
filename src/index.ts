0d4ed016daba80b84c76cbada0a4398791f59fcf
AntonovDev <antonov-dev@mail.ru>
2020-08-09 12:48:51 +0300

    Added models and routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 const port = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+import './core/db';
+
+app.use('*', async (req: express.Request, res: express.Response) => {
+  await res.sendFile(path.join(__dirname, '.', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/models/User.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  email: string,
+  password: string,
+  last_seen?: string,
+  id: string
+}
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: 'Email обязателен!',
+    unique: true
+  },
+  password: {
+    type: String,
+    required: 'Пароль обязателен'
+  },
+  last_seen: {
+    type: Date,
+    default: new Date()
+  }
+}, {
+  timestamps: true
+});
+
+const UserModel = mongoose.model<IUser>('User', UserSchema);
+
+export default UserModel;
